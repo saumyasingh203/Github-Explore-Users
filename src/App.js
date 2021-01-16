fd8dc6801ee8f74f7c97b55755d8609df3c0c26c
Saumya <saumya.singh203@gmail.com>
2021-01-16 22:15:09 +0530

    temp

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem"
+import './App.css';
+
+class App extends React.Component{
+  
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Hello Users" icon="fab fa-github" />
+        <UserItem />
+      </div>
+    );
+  }
+}
+
+export default App;
