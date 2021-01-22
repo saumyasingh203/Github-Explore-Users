28133fc6e7597ecbc1ca4e7a646d6cd03b9f5a38
Saumya <saumya.singh203@gmail.com>
2021-01-22 22:36:04 +0530

    added src folder

---
+++ src/components/users/Users.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Users extends Component {
+
+    state = {
+        users: [
+            
+        ]
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Users
