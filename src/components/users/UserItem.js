2ca4f5ffc843a05141bd90f6fd55d88bb321a8a6
Saumya <saumya.singh203@gmail.com>
2021-01-28 23:58:23 +0530

    more users

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -7,8 +7,17 @@ const UserItem = ({user:{login, avatar_url, html_url}}) => {
                 <img src={avatar_url} className="round-img" style={{ width: "60px" }} alt="" />
                 <h3>{login}</h3>
                 <div><a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
             </div>
+            <img src={avatar_url} className="round-img" style={{ width: "60px" }} alt="" />
+                <h3>{login}</h3>
+                <div><a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+            </div>
+            <img src={avatar_url} className="round-img" style={{ width: "60px" }} alt="" />
+                <h3>{login}</h3>
+                <div><a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+                </div>
+        </div>
+        
     )
 }
 
