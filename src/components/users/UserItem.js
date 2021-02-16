f1179b6094f74e5a8c4ff7f2ee739eadf731d273
Saumya <saumya.singh203@gmail.com>
2021-02-16 19:57:59 +0530

    Useritem js updated

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const UserItem = ({user:{login, avatar_url, html_url}}) => {
-    return (
-            <div className="card text-center">
-                <img src={avatar_url} className="round-img" style={{ width: "60px" }} alt="" />
-                <h3>{login}</h3>
-                <div><a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-            </div>
-            <img src={avatar_url} className="round-img" style={{ width: "60px" }} alt="" />
-                <h3>{login}</h3>
-                <div><a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-            </div>
-            <img src={avatar_url} className="round-img" style={{ width: "60px" }} alt="" />
-                <h3>{login}</h3>
-                <div><a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-                </div>
-        </div>
-        
-    )
-}
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+
+      <div>
+        <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
+          More
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 UserItem.propTypes = {
-    
-}
-export default UserItem
+  user: PropTypes.object.isRequired
+};
+
+export default UserItem;
