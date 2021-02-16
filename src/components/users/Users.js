4b80f094cf96b6a673610253342826cd6f1f2c3c
Saumya <saumya.singh203@gmail.com>
2021-02-16 19:58:39 +0530

    Users js updated

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import GithubContext from '../../context/github/githubContext';
 
-class Users extends Component {
+const Users = () => {
+  const githubContext = useContext(GithubContext);
 
-    state = {
-        users: [
-            
-        ]
-    }
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
-}
+  const { loading, users } = githubContext;
 
-export default Users
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
+};
+
+export default Users;
