05b578bf7e9bed93e0a1a599a3bdf3e6f7bf205e
Saumya Singh <saumya.singh203@gmail.com>
2021-02-23 18:30:57 +0530

    update

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,9 @@ const About = () => {
   return (
     <Fragment>
       <h1>About This App</h1>
-      <p>App to search Github users</p>
+      <p>This is a React App to search Github users</p>
       <p>Version: 1.0.0</p>
+      <p>Created by: Saumya Singh</p>
     </Fragment>
   );
 };
